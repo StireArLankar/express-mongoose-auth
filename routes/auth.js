ac7ea58f552998b81b449f43a37ddccee4e0c1ab
StireArLankar <stire.ru@gmail.com>
2019-08-02 20:35:07 +0300

    added check by existing emails

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,18 @@ router.post('/register', async (req, res) => {
   const { error } = registerValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
+  // const user = await User.findOne({$or:[{ email: req.body.email }, { name: req.body.name }]})
+  // if (user) return res.status(400).send('User already exists')
+
+  const emailExists = await User.findOne({ email: req.body.email })
+  if (emailExists) return res.status(400).send('Email already exists')
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
   })
+
   try {
     const savedUser = await user.save()
     res.send(savedUser)
