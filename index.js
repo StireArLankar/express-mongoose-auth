97fef4ba32c2d3f39719dfe675abf94fbd0b338c
StireArLankar <stire.ru@gmail.com>
2019-08-02 20:11:56 +0300

    connected to db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 
@@ -7,8 +8,14 @@ const port = process.env.SERVER_PORT
 
 const app = express()
 
+mongoose.connect(
+  process.env.MDB,
+  { useNewUrlParser: true },
+  () => console.log('Connected to DB')
+)
+
 app.use(express.json())
 
 app.use('/api/user', authRoute)
 
-app.listen(3000, () => console.log('Server Up and running'))
+app.listen(port, () => console.log('Server Up and running'))
